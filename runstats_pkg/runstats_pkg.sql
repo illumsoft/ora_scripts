32640a5f12dd44fe47a4a9701a5f55376ab00283
user <user@localhost>
2017-05-23 15:30:34 +0300

    runstats_pkg: second version

--- runstats_pkg/runstats_pkg.sql
+++ runstats_pkg/runstats_pkg.sql
@@ -34,6 +34,17 @@ as
   procedure rs_start;
   procedure rs_middle;
   procedure rs_stop(p_difference_threshold in number default 0);
+
+  procedure clear_runs_stats;
+  procedure run1_begin;
+  procedure run1_end;
+  procedure run1_show(p_value_threshold in number default 0);
+
+  procedure run2_begin;
+  procedure run2_end;
+  procedure run2_show(p_value_threshold in number default 0);
+
+  procedure show_diff(p_difference_threshold in number default 0);
 end runstats_pkg;
 /
 ------------------------------------------------------------------------
@@ -155,6 +166,186 @@ $end
       dbms_output.put_line(x.diff);
     end loop;
   end rs_stop;
+
+  procedure clear_runs_stats
+  as
+  begin
+    delete from run_stats;
+  end clear_runs_stats;
+
+  procedure run1_begin
+  as
+  begin
+    -- delete from run_stats where substr(runid, 1, 5) = 'RUN1_';
+    insert into run_stats select 'RUN1_BEGIN', stats.* from stats;
+    g_start := dbms_utility.get_time;
+  end run1_begin;
+
+  procedure run1_end
+  as
+  begin
+    g_run1 := (dbms_utility.get_time - g_start);
+    insert into run_stats select 'RUN1_END', stats.* from stats;
+  end run1_end;
+
+  procedure run2_begin
+  as
+  begin
+    -- delete from run_stats where substr(runid, 1, 5) = 'RUN2_';
+    insert into run_stats select 'RUN2_BEGIN', stats.* from stats;
+    g_start := dbms_utility.get_time;
+  end run2_begin;
+
+  procedure run2_end
+  as
+  begin
+    g_run2 := (dbms_utility.get_time - g_start);
+    insert into run_stats select 'RUN2_END', stats.* from stats;
+  end run2_end;
+
+  procedure show_diff(p_difference_threshold in number default 0)
+  as
+    v_name_len  constant pls_integer := 40;
+    v_value_len          pls_integer;
+    v_format             varchar2(40);
+  begin
+    -- Prepare formats
+    select max(length(to_char(abs(value)))) into v_value_len from run_stats;
+    v_format := rpad('9', v_value_len, '9');
+
+    -- Output values
+    dbms_output.put_line('Run1 ran in ' || g_run1 || ' hsecs');
+    dbms_output.put_line('Run2 ran in ' || g_run2 || ' hsecs');
+    dbms_output.put_line('Run1 is ' || round(100 * g_run1 / nullif(g_run2, 0), 2) || '% of the Run2 time');
+    dbms_output.new_line;
+    dbms_output.put_line(
+         rpad('Name', v_name_len)
+      || lpad('Run1', v_value_len + 1)
+      || lpad('Run2', v_value_len + 1)
+      || lpad('Diff', v_value_len + 1) || ' (Run2 - Run1)');
+
+    for x in (
+      select rpad(rs.name, v_name_len)
+          || to_char(rs.run1_end - rs.run1_begin, v_format)
+          || to_char(rs.run2_end - rs.run2_begin, v_format)
+          || to_char(((rs.run2_end - rs.run2_begin) - (rs.run1_end - rs.run1_begin)), v_format) diff
+      from run_stats
+      pivot (
+        max(value) for runid in (
+          'RUN1_BEGIN' as run1_begin, 'RUN1_END' as run1_end,
+          'RUN2_BEGIN' as run2_begin, 'RUN2_END' as run2_end)
+      ) rs
+      where abs((rs.run2_end - rs.run2_begin) - (rs.run1_end - rs.run1_begin)) > p_difference_threshold
+      order by substr(rs.name, 1, 4), abs((rs.run2_end - rs.run2_begin) - (rs.run1_end - rs.run1_begin))
+    )
+    loop dbms_output.put_line(x.diff); end loop;
+    dbms_output.new_line;
+
+    dbms_output.put_line('Total Run1 latches versus Run2 - difference and percent');
+    dbms_output.put_line(
+         lpad('Run1', v_value_len + 1)
+      || lpad('Run2', v_value_len + 1)
+      || lpad('Diff', v_value_len + 1)
+      || lpad('Pct', 8));
+
+    for x in (
+      select to_char(run1, v_format)
+          || to_char(run2, v_format)
+          || to_char(diff, v_format)
+          || to_char(round(100 * run1 / run2, 2), '999.99') || '%' diff
+      from (select sum(rs.run1_end - rs.run1_begin) run1,
+                   sum(rs.run2_end - rs.run1_begin) run2,
+                   sum((rs.run2_end - rs.run2_begin) - (rs.run1_end - rs.run1_begin)) diff
+            from run_stats
+            pivot (
+              max(value) for runid in (
+                'RUN1_BEGIN' as run1_begin, 'RUN1_END' as run1_end,
+                'RUN2_BEGIN' as run2_begin, 'RUN2_END' as run2_end
+              )
+            ) rs
+            where rs.name like 'LATCH%'
+           )
+    )
+    loop dbms_output.put_line(x.diff); end loop;
+
+  end show_diff;
+
+  procedure run1_show(p_value_threshold in number default 0)
+  as
+    v_name_len  constant pls_integer := 40;
+    v_value_len          pls_integer;
+    v_format             varchar2(40);
+  begin
+    -- Prepare formats
+    select max(length(to_char(abs(value)))) into v_value_len from run_stats;
+    v_format := rpad('9', v_value_len, '9');
+
+    -- Output values
+    dbms_output.put_line('Run1 ran in ' || g_run1 || ' hsecs');
+    dbms_output.new_line;
+    dbms_output.put_line(rpad('Name', v_name_len) || lpad('Run1', v_value_len + 1));
+    for x in (
+      select rpad(rs.name, v_name_len) || to_char(rs.run1_end - rs.run1_begin, v_format) diff
+      from run_stats
+      pivot (max(value) for runid in ('RUN1_BEGIN' as run1_begin, 'RUN1_END' as run1_end)) rs
+      where abs(rs.run1_end - rs.run1_begin) > p_value_threshold
+      order by substr(rs.name, 1, 4), abs((rs.run1_end - rs.run1_begin))
+    )
+    loop dbms_output.put_line(x.diff); end loop;
+
+    dbms_output.new_line;
+    dbms_output.put('Total Run1 latches: ');
+    for x in (
+      select to_char(run1, v_format) diff
+      from (
+        select sum(rs.run1_end - rs.run1_begin) run1
+        from run_stats
+        pivot (max(value) for runid in ( 'RUN1_BEGIN' as run1_begin, 'RUN1_END' as run1_end)) rs
+        where rs.name like 'LATCH%'
+      )
+    )
+    loop dbms_output.put_line(x.diff); end loop;
+
+  end run1_show;
+
+  procedure run2_show(p_value_threshold in number default 0)
+  as
+    v_name_len  constant pls_integer := 40;
+    v_value_len          pls_integer;
+    v_format             varchar2(40);
+  begin
+    -- Prepare formats
+    select max(length(to_char(abs(value)))) into v_value_len from run_stats;
+    v_format := rpad('9', v_value_len, '9');
+
+    -- Output values
+    dbms_output.put_line('Run2 ran in ' || g_run2 || ' hsecs');
+    dbms_output.new_line;
+    dbms_output.put_line(rpad('Name', v_name_len) || lpad('Run2', v_value_len + 1));
+    for x in (
+      select rpad(rs.name, v_name_len) || to_char(rs.run2_end - rs.run2_begin, v_format) diff
+      from run_stats
+      pivot (max(value) for runid in ('RUN2_BEGIN' as run2_begin, 'RUN2_END' as run2_end)) rs
+      where abs(rs.run2_end - rs.run2_begin) > p_value_threshold
+      order by substr(rs.name, 1, 4), abs((rs.run2_end - rs.run2_begin))
+    )
+    loop dbms_output.put_line(x.diff); end loop;
+
+    dbms_output.new_line;
+    dbms_output.put('Total Run2 latches: ');
+    for x in (
+      select to_char(run2, v_format) diff
+      from (
+        select sum(rs.run2_end - rs.run2_begin) run2
+        from run_stats
+        pivot (max(value) for runid in ( 'RUN2_BEGIN' as run2_begin, 'RUN2_END' as run2_end)) rs
+        where rs.name like 'LATCH%'
+      )
+    )
+    loop dbms_output.put_line(x.diff); end loop;
+
+  end run2_show;
+
 end runstats_pkg;
 /
 ------------------------------------------------------------------------
